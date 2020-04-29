2cbde1e4eaca101e8ee73f2dee5b4d5a595fc17c
Rafal Samek <rafal.samek@gmail.com>
2020-04-29 02:17:37 +0200

    Initial commit
    Signed-off-by: Rafal Samek <rafal.samek@gmail.com>

---
+++ tests/AppBundle/Entity/EnclosureTest.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace Tests\AppBundle\Entity;
+
+
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
+use AppBundle\Exception\DinosaursAreRunningRampantException;
+use AppBundle\Exception\NotABuffetException;
+
+use PHPUnit\Framework\TestCase;
+
+class EnclosureTest extends TestCase
+{
+    public function testItHasNoDinosaursByDefault()
+    {
+        $enclusure = new Enclosure();
+
+        $this->assertEmpty($enclusure->getDinosaurs());
+    }
+
+    public function testItAddsDinosaurs()
+    {
+        $enclosure = new Enclosure(true);
+
+        $enclosure->addDinosaur(new Dinosaur());
+        $enclosure->addDinosaur(new Dinosaur());
+
+        $this->assertCount(2, $enclosure->getDinosaurs());
+    }
+
+    public function testItDoesNotAllowCarnivorousDinosaursToMixWithHerbivores()
+    {
+        $enclosure = new Enclosure(true);
+        $enclosure->addDinosaur(new Dinosaur());
+
+        $this->expectException(NotABuffetException::class);
+
+        $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
+    }
+
+    /**
+     * @expectedException  \AppBundle\Exception\NotABuffetException
+     */
+    public function testItDoesNotAllowNonCarnivorousDinosaursToCarnivorousEnclosure()
+    {
+        $enclosure = new Enclosure(true);
+        $enclosure->addDinosaur(new Dinosaur('Velociraptor', true));
+        $enclosure->addDinosaur(new Dinosaur());
+    }
+
+    public function testItDoesNotAllowToAddDinosaursToUnsecureEnclosures()
+    {
+        $enclosure = new Enclosure();
+
+        $this->expectException(DinosaursAreRunningRampantException::class);
+        $this->expectExceptionMessage('Are you craaazy?!?');
+
+        $enclosure->addDinosaur(new Dinosaur());
+    }
+}
---
+++ tests/AppBundle/Factory/DinosaurFactoryTest.php
@@ -0,0 +1,81 @@
+<?php
+
+
+namespace Tests\AppBundle\Factory;
+
+
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Factory\DinosaurFactory;
+use AppBundle\Service\DinosaurLengthDeterminator;
+use PHPUnit\Framework\TestCase;
+
+class DinosaurFactoryTest extends TestCase
+{
+
+    /**
+     * @var DinosaurFactory
+     */
+    private $factory;
+
+    /**
+     * @var DinosaurLengthDeterminator|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $lengthDeterminator;
+
+    protected function setUp()
+    {
+        $this->lengthDeterminator = $this->createMock(DinosaurLengthDeterminator::class);
+        $this->factory = new DinosaurFactory($this->lengthDeterminator);
+    }
+
+    public function testItGrowsAVelociraptor()
+    {
+        $dinosaur = $this->factory->growVelociraptor(5);
+
+        $this->assertInstanceOf(Dinosaur::class, $dinosaur);
+        $this->assertInternalType('string', $dinosaur->getGenus());
+        $this->assertSame('Velociraptor', $dinosaur->getGenus());
+        $this->assertSame(5, $dinosaur->getLength());
+    }
+
+    public function testItGrowsATriceratops()
+    {
+        $this->markTestIncomplete('Waiting for confirmation from GenLab.');
+    }
+
+    public function testItGrowsABabyVelociraptor()
+    {
+        if (!class_exists('Nanny')) {
+            $this->markTestSkipped('There is nobody to watch the baby');
+        }
+        $dinosaur = $this->factory->growVelociraptor(1);
+        $this->assertSame(1, $dinosaur->getLength());
+    }
+
+    /**
+     * @dataProvider getSpecificationTests
+     *
+     */
+    public function testItGrowsADinosaurFromASpecification(string $spec, bool $expectedIsCarnivorous)
+    {
+        $this->lengthDeterminator->expects($this->once())
+            ->method('getLengthFromSpecification')
+            ->with($spec)
+            ->willReturn(20);
+        $dinosaur = $this->factory->growFromSpecification($spec);
+
+        $this->assertSame($expectedIsCarnivorous, $dinosaur->isCarnivorous(), 'Diets do not match');
+        $this->assertSame(20, $dinosaur->getLength());
+    }
+
+    public function getSpecificationTests()
+    {
+        return [
+            //specfication, is large, is carnivorous
+            ['large carnivorous dinosaur', true],
+            'default_response' => ['give me all the cookies', false],
+            ['large herbivore', false],
+            ['small non-carnivorous dino', false],
+        ];
+    }
+}
